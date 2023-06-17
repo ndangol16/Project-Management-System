f9c1c3f7986de6a1eb2cf1d21c8fbc531efcb82e
Nirnaya Dangol <dnirnaya@gmail.com>
2023-06-17 10:15:39 +0545

    changed server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 
 const app = express();
@@ -11,8 +13,19 @@ const PORT = process.env.PORT || 8080;
 // logs
 app.use(morgan("tiny"));
 
-app.get('/',(req,res)=>{
-    res.send("TrellNOOO App");
-});
+// parse request
+app.use(express.urlencoded({extended : true}));
+
+
+
+// load assets
+app.use('/css',express.static(__dirname + "assets/css"));
+app.use('/js',express.static(__dirname + "assets/js"));
+app.use('/img',express.static(__dirname + "assets/img"));
+// app.use('/js',express.static(path.resolve(__dirname,"assets/js")));
+// app.use('/img',express.static(path.resolve(__dirname,"assets/img")));
+
+// load routers
+app.use('/',require('./server/routes/router'));
 
 app.listen(PORT,()=>{console.log(`Sevrer is running on http://localhost:${PORT}`)});
